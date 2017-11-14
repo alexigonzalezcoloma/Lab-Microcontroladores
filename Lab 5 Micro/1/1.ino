e34ee791ba518ae4885eead058837236b243a2dd
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2017-11-14 05:49:39 -0300

    prot

---
+++ Lab 5 Micro/1/1.ino
@@ -0,0 +1,21 @@
+int potenciometro1;
+int potenciometro2;
+int potenciometro3;
+String pot;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  potenciometro1=-1;
+  potenciometro2=-1;
+  potenciometro3=-1;
+}
+
+void loop() {
+  potenciometro1 = analogRead(A0);
+  potenciometro2 = analogRead(A1);
+  potenciometro3 = analogRead(A2);
+  pot=String(potenciometro1)+','+String(potenciometro2)+','+String(potenciometro3)+',';
+  Serial.println(pot);
+
+}
