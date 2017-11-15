e4f894d0bdf148821700ea323ee7a99e342194c1
Alexi Gonzalez <agonzalez2016@alu.uct.cl>
2017-11-15 11:05:54 -0300

    ard

--- Lab 5 Micro/1-4.py
+++ Lab 5 Micro/1-4.py
@@ -1,11 +1,14 @@
 from __future__ import division
 from visual import *
+import serial
 
 Panta     = display(title='Brazo Robotico',x=50,y=0,width=640,height=640,center=(0,0,0))
 frameBase = frame(pos = (0,0,0))
 Base      = box(frame=frameBase,size=(1000,6,1000),color=color.white)
 
 
+def  MAP(x,vi,vf,vis,vfs):
+    return(x-vi)*(vfs-vis)/(vf-vi)+vis
 
 def Base():
     f=frame(pos=(0,0,0))
@@ -35,6 +38,7 @@ def Brazo3(f):
     f4.box1 =      box(frame=f4, pos=(0,0,60), length=40, height=40,  width=40,color=color.green)
     f4.line =      box(frame=f4, pos=(0,0,70), length=10, height=100, width=10,color=color.white)
     return f4
+puerto=serial.Serial(0,9600)
     
 parte1 = Base()
 parte2 = Brazo1(parte1)
@@ -42,13 +46,18 @@ parte3 = Brazo2(parte2)
 parte4 = Brazo3(parte3)
 fr     = frame(pos = (0,0,0))
 b=0
+# part 1  de 0 a 360 part 2 de 0 a45 part3 -90 a 45 part4 de 0 a 360
+parte2.rotate(angle = math.radians(45), axis=(1,0,0), origin = (39,118,-25))
+parte3.rotate(angle = math.radians(45), axis=(1,0,0), origin = (0 ,140,-10))
+parte4.rotate(angle = math.radians(), axis=(0,0,1), origin = parte4.pos )
 
 while True:
-    #get= Panta.mouse.getclick()  ;   print get.pos
-    rate(10);
-
-    
-    parte1.rotate(angle = math.radians(2), axis=(0,1,0), origin = parte1.pos)
-    parte2.rotate(angle = math.radians(2), axis=(1,0,0), origin = (39,118,-25))
-    parte3.rotate(angle = math.radians(2), axis=(1,0,0), origin = (0 ,140,-10))
-    parte4.rotate(angle = math.radians(2), axis=(0,0,1), origin = parte4.pos  )
+    grados_1=int(puerto.readline()[0])
+    grados_2=int(puerto.readline()[1])
+    grados_3=int(puerto.readline()[2])
+    grados_4=int(puerto.readline()[3])
+    rate(10);    
+    #parte1.rotate(angle = math.radians(2), axis=(0,1,0), origin = parte1.pos)
+    parte2.rotate(angle = math.radians(), axis=(1,0,0), origin = (39,118,-25))
+    #parte3.rotate(angle = math.radians(), axis=(1,0,0), origin = (0 ,140,-10))
+    #parte4.rotate(angle = math.radians(), axis=(0,0,1), origin = parte4.pos  )
